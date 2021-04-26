a41b33d00cbeababe60d395673199363088adf47
KZTahat <kztahat96@gmail.com>
2021-04-26 18:06:28 +0300

    adding css and doc.write on screen

--- js/app.js
+++ js/app.js
@@ -1,26 +1,30 @@
 'use strict'
 
-let qustion1 = prompt('please inter your name?/');
-if (qustion1 != null) {
-    alert('Welcom', qustion1);
-    console.log('Name: ',qustion1);
+let username = prompt('please inter your name?');
+if (username != null) {
+    alert('Welcom ' + username);
+    console.log('Name: ' + username);
+    document.write('Welcom ' + username);
 }
 
-let qustion2 = Number(prompt('where are you from ',qustion1,'?'));
-if(qustion2!=null){
-    alert('great country',qustion2);
-    console.log('country: ',qustion2);
+let country = prompt('where are you from ', username, '?');
+if (country != null) {
+    alert('great country ' + country);
+    console.log('country: ' + country);
+    document.write('country :' + country);
 }
 
-let qustion3 = prompt('what is your best food type?');
-if(qustion3!=null){
+let foodType = prompt('what is your best food type?');
+if (foodType != null) {
     alert('I think you have a good taste');
-    console.log('best food type: ',qustion3);
+    console.log('best food type: ' + foodType);
+    document.write('foodType :' + foodType);
 }
 
-let qustion4 = prompt('what is your best hobby?');
-if(qustion4!=null){
+let hobby = prompt('what is your best hobby?');
+if (hobby != null) {
     alert('I think we are on the same page');
-    console.log('hobby: ',qustion4);
+    console.log('hobby: ' + hobby);
+    document.write('hobby :' + hobby);
 }
 
